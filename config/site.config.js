0fcb4dae92da68cd6648e0f8390b225887ff31ed
always0e0w <saltedfish.me@gmail.com>
2022-05-24 23:29:39 +0800

    update domain

--- config/site.config.js
+++ config/site.config.js
@@ -14,7 +14,7 @@ module.exports = {
   icon: '/icons/192.png',
 
   // The name of your website. Present alongside your icon.
-  title: "White hole's OneDrive",
+  title: "AlphaBeta's OneDrive",
 
   // The folder that you are to share publicly with onedrive-vercel-index. Use '/' if you want to share your root folder.
   baseDirectory: '/share',
@@ -50,7 +50,7 @@ module.exports = {
   links: [
     {
       name: 'Blog',
-      link: 'https://whitehole.dev/'
+      link: 'https://alphabeta.top/'
     },
     {
       name: 'Telegram',
